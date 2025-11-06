3ec95bc34724ddefb9592e1c71ec7f0ae4394a7a
HNikkel <156240754+HNikkel@users.noreply.github.com>
2025-11-06 09:31:23 -0800

    Fixes client.js

--- client.js
+++ client.js
@@ -23,7 +23,7 @@ pgclient.query(text, values, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query('SELECT * FROM student', (err, res) => {
+pgclient.query('SELECT * FROM Profile', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print the data in student table
     pgclient.end()
